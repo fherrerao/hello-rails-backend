cefa435c0f6dd6054fe9bca0cb578dbd301f90c2
fherrerao <fherrera0206@gmail.com>
2022-06-13 22:19:10 -0500

    Add routes to  greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :v1, defaults: { format: :json } do
-    resources :greetings, only: [:index]
-  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
+  resources :greetings, only: [:index]  
 
   # Defines the root path route ("/")
   root "greetings#index"
